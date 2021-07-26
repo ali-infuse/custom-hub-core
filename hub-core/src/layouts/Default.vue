2057843ef9a7b04d12c8c86b91fc2b3ab8907bc0
ali-infuse <87978898+ali-infuse@users.noreply.github.com>
2021-07-26 15:31:56 +0400

    testing default layout update by updating first fork

--- hub-core/src/layouts/Default.vue
+++ hub-core/src/layouts/Default.vue
@@ -8,8 +8,8 @@
       </div>
       <footer class="mt-8 mb-12 ml-24 mr-12">
         <div class="flex items-center justify-end">
-          <get-candy-stamp class="w-6 mr-2" />
-          <a href="https://www.getcandy.io/" class="text-sm text-gray-500" target="_blank" rel="noopener">GetCandy Hub v{{ apiVersion }}</a>
+          <!-- <get-candy-stamp class="w-6 mr-2" /> -->
+          <!-- <a href="https://www.getcandy.io/" class="text-sm text-gray-500" target="_blank" rel="noopener">GetCandy Hub v{{ apiVersion }}</a> -->
         </div>
       </footer>
       <div v-if="notify.pending.length" class="fixed bottom-0 right-0 z-50 flex justify-end w-full p-4 pointer-events-none">
